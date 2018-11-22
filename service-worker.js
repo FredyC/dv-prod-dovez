c143620f196f3b5d523f059b50e7d3d75f885267
Daniel K <fredy.c@seznam.cz>
2018-11-22 12:31:13 +0100

    Release 01e1030e472c94c00096e11f1f72a8268b7ebf93

--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,7 @@ importScripts("/workbox-v3.6.3/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/workbox-v3.6.3"});
 
 importScripts(
-  "/precache-manifest.82b6d4e65be48288c0cbb4165163dcb3.js"
+  "/precache-manifest.48a4a9b320b8eb6e2cbd8763b86a3cd9.js"
 );
 
 workbox.clientsClaim();
